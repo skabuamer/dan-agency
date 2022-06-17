9280e5eca911a7e10d4e14583fa747d843a2bc28
skabuamer <shaikhabuamer@gmail.com>
2022-06-17 18:33:36 +0600

    first-commit

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.single-banner').slick();
+    
+  });
+
+  jQuery(document).ready(function($) {
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+        });
+    });
